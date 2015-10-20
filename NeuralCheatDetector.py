e5ef57ce1b2a59fc0e99275312215135114a6b0f
clarkerubber <jamesjosephclarke@gmail.com>
2015-10-21 02:09:56 +1100

    Trim fat and add comments

--- NeuralCheatDetector.py
+++ NeuralCheatDetector.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-from pybrain.tools.customxml import NetworkWriter, NetworkReader
+from pybrain.tools.customxml import NetworkReader
 from pybrain.datasets import ClassificationDataSet
 import json, cgi
 
@@ -65,10 +65,12 @@ input_text = """[{
 	"blurs":0
 }]"""
 
+# Get posted data
 form = cgi.FieldStorage()
 
 input_data = json.loads(form.getvalue("input", input_text))
 
+# Convert data from JSON to DataSet for processing
 player_assessments = ClassificationDataSet(11, 1, nb_classes=2)
 
 for assessment in input_data:
@@ -86,68 +88,30 @@ for assessment in input_data:
 		float(assessment["mtSd"] / assessment["mtAvg"])),
 		[1])
 
+# Instantiate/Load first neural network
 net_first_pass = NetworkReader.readFrom('FirstPass.xml')
 
-first_pass = ClassificationDataSet(40, 1, nb_classes=2)
-
+# Process and rank games in terms of probability of cheating,
+# then pad results with least suspicious result if there are less than 20 games
 temp = sorted(net_first_pass.activateOnDataset(player_assessments), cmp=lambda x, y: cmp(y[1], x[1]))[:20]
 temp += [temp[-1]] * (20 - len(temp))
-temp = [val for sublist in temp for val in sublist]
+temp = [val for sublist in temp for val in sublist] # Flatten list of lists
 
-first_pass.addSample((
-		temp[0],
-		temp[1],
-		temp[2],
-		temp[3],
-		temp[4],
-		temp[5],
-		temp[6],
-		temp[7],
-		temp[8],
-		temp[9],
-		temp[10],
-		temp[11],
-		temp[12],
-		temp[13],
-		temp[14],
-		temp[15],
-		temp[16],
-		temp[17],
-		temp[18],
-		temp[19],
-		temp[20],
-		temp[21],
-		temp[22],
-		temp[23],
-		temp[24],
-		temp[25],
-		temp[26],
-		temp[27],
-		temp[28],
-		temp[29],
-		temp[30],
-		temp[31],
-		temp[32],
-		temp[33],
-		temp[34],
-		temp[35],
-		temp[36],
-		temp[37],
-		temp[38],
-		temp[39]), [1])
-
+# Instantiate/Load second neural network
 net_second_pass = NetworkReader.readFrom('SecondPass.xml')
 
-second_pass = net_second_pass.activateOnDataset(first_pass)
+# Process results from first neural network
+second_pass = net_second_pass.activate(temp)
 
-tolerance = 0.7 # 0 = No reports, strictly marking or not marking. 1 = all reports, no marking/non-marking
+# Determine what should be done to the player and return results
+tolerance = 0.6 # 0 = No reports, strictly marking or not marking. 1 = all reports, no marking/non-marking
 
 print "Content-type: text/html"
 print
 
-if second_pass[0][0] > second_pass[0][1] - tolerance and second_pass[0][0] < second_pass[0][1] + tolerance:
+if second_pass[0] > second_pass[1] - tolerance and second_pass[0] < second_pass[1] + tolerance:
 	print "REPORT"
-elif second_pass[0][0] > second_pass[0][1]:
+elif second_pass[0] > second_pass[1]:
 	print "NO-ACTION"
 else:
 	print "MARK"
