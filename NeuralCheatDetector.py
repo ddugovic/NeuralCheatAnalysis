c4055fdc2df497cc46aaa19344ebdb1d63690b1e
clarkerubber <jamesjosephclarke@gmail.com>
2016-02-14 03:15:33 +1100

    Let's see if we can get the error to show itself

--- NeuralCheatDetector.py
+++ NeuralCheatDetector.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python
-from pymongo import MongoClient
 from pybrain.tools.customxml import NetworkReader
 from pybrain.datasets import ClassificationDataSet
 import json, cgi
+
+print "Content-type: application/json"
+print
+
+from pymongo import MongoClient
 
 input_text = """[{
 	"_id":"VxrF4mh9",
@@ -634,9 +638,6 @@ input_data = json.loads(form.getvalue("input", input_text))
 #collection = db.gameData
 
 #collection.insert(input_data)
-
-print "Content-type: application/json"
-print
 
 output = {'decision': json.dumps(input_data), 'cheatPercent': 20}
 print json.dumps(output)
