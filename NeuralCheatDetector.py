4a2158a9b12ad83817acbf5e898fba0abea73769
clarkerubber <jamesjosephclarke@gmail.com>
2016-02-13 23:41:40 +1100

    Attempt to use mongodb

--- NeuralCheatDetector.py
+++ NeuralCheatDetector.py
@@ -1,25 +1,34 @@
 #!/usr/bin/python
+from pymongo import MongoClient
 from pybrain.tools.customxml import NetworkReader
 from pybrain.datasets import ClassificationDataSet
 import json, cgi
 
 input_text = """[{
-	"ser":0,
-	"aha":0,
-	"cmt":0,
-	"nfm":0,
-	"sha": 0,
-	"sfAvg":82,
-	"sfSd":130,
-	"mtAvg":46,
-	"mtSd":34,
-	"blurs":0
-}]"""
+	"evals": [-15, -8, -9, -32, -37, -110, -173, -274, -610, -720, -690, -957, -719, -702, -1000, -1000, -1000, -1000, -1000, -1000, -1000, -1000, -1000, -1000, -1000, -1000, -1000, -1000, -1000, -1000, -1000, -1000, -1000, -1000, -1000],
+	"diffs": [2, 8, 0, 27, 3, 43, 19, 107, 0, 91, 3, 256, 26, 0, 0, 0, 15, 0, 0, 11, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	"moveTimes": [5, 1, 1, 1, 1, 1, 10, 1, 10, 1, 5, 5, 5, 10, 15, 5, 10, 10, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+	"holdAlert": false,
+	"isProvisional": false,
+	"rating": 2448,
+	"isWinner": true, 
+	"blurs": 0, 
+	"cpAvg": 17
+}"""
 
 # Get posted data
 form = cgi.FieldStorage()
 
 input_data = json.loads(form.getvalue("input", input_text))
+
+# Do MongoDB stuff to save data
+client = MongoClient()
+
+db = client.tensorFlow
+collection = db.gameData
+posts = db.posts
+
+posts.save(input_data)
 
 print "Content-type: application/json"
 print
